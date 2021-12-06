c8b99442cf91d2119a82b8736f85ae9835b1c027
Akhil varghese <akhil.varghese@student.griffith.ie>
2021-12-06 19:47:22 +0000

    Bracket 2

--- app/src/main/java/com/assignment/compassgps/CompassView.kt
+++ app/src/main/java/com/assignment/compassgps/CompassView.kt
@@ -10,6 +10,7 @@ import kotlin.math.min
 
 class CompassView(context: Context?, attrs: AttributeSet?) : View(context, attrs) {
 
+    private var rotationDegreeValue = 0f
     private var north: String? = null
     private var south: String? = null
     private var east: String? = null
@@ -24,6 +25,7 @@ class CompassView(context: Context?, attrs: AttributeSet?) : View(context, attrs
     private var px = 0
     private var py = 0
     private var textHeight = 0
+
 
     init {
         black = Paint(Paint.ANTI_ALIAS_FLAG)
@@ -66,64 +68,89 @@ class CompassView(context: Context?, attrs: AttributeSet?) : View(context, attrs
     override fun onDraw(canvas: Canvas?) {
         super.onDraw(canvas)
         canvas ?: return
+
         var width: Int = canvas!!.width
         var height: Int = canvas!!.height
+        canvas?.save()
+        canvas.rotate(rotationDegreeValue, px.toFloat(), py.toFloat())
+        canvas?.restore()
         drawCompassCircle(canvas)
-        drawCompassPoles(canvas)
         drawFaceLine(canvas)
 
-    }
 
-    private fun drawCompassPoles(canvas: Canvas) {
-        canvas?.save()
-        val textWidth = 10
-        val cadinalX = px - textWidth / 2
-        val cadinalY = py - radius - 60
-        for (i in 0..3) {
-            canvas.drawLine(px.toFloat(), (px - radius).toFloat(), py.toFloat(), (py - radius + 50).toFloat(), red!!)
-            canvas.save()
-            canvas.translate(0f, 50.toFloat())
-            when (i) {
-                0 -> dirString = north.toString()
-                1 -> dirString = east.toString()
-                2 -> dirString = west.toString()
-                3 -> dirString = south.toString()
-                else -> {
-                }
-            }
-            if(i==0){
-                canvas.drawText(dirString!!, cadinalX.toFloat(), cadinalY.toFloat(), red!!)
-            }
-            else{
-                canvas.drawText(dirString!!, cadinalX.toFloat(), cadinalY.toFloat(), dkBlack!!)
-            }
-
-            canvas.restore()
-            canvas.rotate(90f, px.toFloat(), py.toFloat())
-        }
-        canvas.restore()
     }
 
     private fun drawFaceLine(canvas: Canvas) {
 
         canvas?.save()
-        canvas.drawLine(px.toFloat(), py.toFloat(),
+        /*canvas.drawLine(px.toFloat(), py.toFloat(),
             (px + radius * Math.sin(-direction.toDouble())).toFloat(),
             (py - radius * Math.cos(-direction.toDouble())).toFloat(),
             black
-        )
+        )*/
+    canvas.drawLine(px.toFloat(), py.toFloat(),
+           (px + radius * Math.sin(-direction.toDouble())).toFloat(),
+           (py - radius * Math.cos(-direction.toDouble())).toFloat(),
+           black
+       )
         canvas?.restore()
     }
 
     public fun drawCompassCircle(canvas: Canvas) {
         px = canvas.width / 2
         py = canvas.height / 2
-        radius = px -50
+        radius = px -80
+        val textWidth = 10
+        val cadinalX = px - textWidth / 2
+        val cadinalY = py - radius - 60
+
         canvas?.save()
 
         canvas.drawCircle(px.toFloat(), py.toFloat(), radius.toFloat(), black)
         canvas.drawCircle(px.toFloat(), py.toFloat(), radius/2.toFloat(), ltblack)
+
+
+        for (i in 0..3) {
+            canvas.drawLine(px.toFloat(), (px - radius).toFloat(), py.toFloat(), (py - radius + 50).toFloat(), red!!)
+            canvas.save()
+            canvas.translate(0f, 0f)
+            when (i) {
+                0 -> dirString = north.toString()
+                1 -> dirString = east.toString()
+                2 -> dirString = south.toString()
+                3 -> dirString = west.toString()
+                else -> {
+                }
+            }
+          /*  if(i==0){
+                canvas.drawText(dirString!!, cadinalX.toFloat(), cadinalY.toFloat(), red!!)
+            }
+            else{
+                canvas.drawText(dirString!!, cadinalX.toFloat(), cadinalY.toFloat(), dkBlack!!)
+            }
+
+*/
+            if(i==0){
+                canvas.drawText(dirString!!,(px + radius * Math.sin((-rotationDegreeValue).toDouble() / 180 * Math.PI)).toFloat(),
+                    (py - radius * Math.cos((-rotationDegreeValue).toDouble() / 180 * Math.PI)).toFloat(), red!!)
+            }
+            else{
+                canvas.drawText(dirString!!,(px + radius * Math.sin((-rotationDegreeValue).toDouble() / 180 * Math.PI)).toFloat(),
+                    (py - radius * Math.cos((-rotationDegreeValue).toDouble() / 180 * Math.PI)).toFloat(), dkBlack!!)
+
+            }
+            canvas.restore()
+            canvas.rotate(90f, this.px.toFloat(), this.py.toFloat())
+        }
         canvas.restore()
 
     }
+
+
+    fun updateCompassRotaion(rotationDegreeValue: Float) {
+        this.rotationDegreeValue = rotationDegreeValue
+        invalidate()
+    }
 }
+
+
--- app/src/main/java/com/assignment/compassgps/MainActivity.kt
+++ app/src/main/java/com/assignment/compassgps/MainActivity.kt
@@ -1,11 +1,57 @@
 package com.assignment.compassgps
 
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var compassViewData: CompassView
+    private lateinit var senesorManager: SensorManager
+    var rtnMatrix: FloatArray = FloatArray(16) { 0f }
+    var orientationValue: FloatArray = FloatArray(4) { 0f }
+    public var rtnValue: Float = 0.0f
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        compassViewData = findViewById<View>(R.id.compass_view) as CompassView
+        senesorManager = getSystemService(SENSOR_SERVICE) as SensorManager
+        addRotationListener()
+
+    }
+
+    private fun addRotationListener() {
+
+        if (senesorManager.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR) != null) {
+            senesorManager.registerListener(
+                object : SensorEventListener {
+                    override fun onAccuracyChanged(p0: Sensor?, p1: Int) {
+                    }
+
+                    override fun onSensorChanged(p0: SensorEvent?) {
+
+                        SensorManager.getRotationMatrixFromVector(rtnMatrix, p0!!.values)
+                        SensorManager.getOrientation(rtnMatrix, orientationValue)
+                        orientationValue[0] =
+                            Math.toDegrees(orientationValue[0].toDouble()).toFloat()
+                        orientationValue[1] =
+                            Math.toDegrees(orientationValue[1].toDouble()).toFloat()
+                        orientationValue[2] =
+                            Math.toDegrees(orientationValue[2].toDouble()).toFloat()
+
+                        rtnValue = orientationValue[0]
+                        compassViewData.updateCompassRotaion(orientationValue[0])
+                    }
+                }, senesorManager.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR),
+                SensorManager.SENSOR_DELAY_UI
+            )
+        }
     }
 }
